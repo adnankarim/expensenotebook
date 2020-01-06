ddb1a5b15c07949476c5f0755389dfd0377caef0
adnankarim <skiwordylectures@gmail.com>
2020-01-06 21:49:44 +0500

    Updates

---
+++ precache-manifest.f913f8810b75489229f35df244301f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1ecd58be9c6f42a6d2c0709cd3ad28",
+    "url": "/expensenotebook/index.html"
+  },
+  {
+    "revision": "158f3dfa3378250aa458",
+    "url": "/expensenotebook/static/css/main.87617b18.chunk.css"
+  },
+  {
+    "revision": "d5f49a202c979bc59960",
+    "url": "/expensenotebook/static/js/2.7596644d.chunk.js"
+  },
+  {
+    "revision": "b851b380d8cc12c3c895f54a4c7937f5",
+    "url": "/expensenotebook/static/js/2.7596644d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "158f3dfa3378250aa458",
+    "url": "/expensenotebook/static/js/main.6bbf1a1d.chunk.js"
+  },
+  {
+    "revision": "a061f4ccefd90f146611",
+    "url": "/expensenotebook/static/js/runtime-main.82c79831.js"
+  }
+]);
